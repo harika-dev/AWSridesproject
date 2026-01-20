3d3edf3fe865f9ec7dab03739c77ec5f36c0218e
Harika <148167161+harika-dev@users.noreply.github.com>
2026-01-20 13:37:04 -0600

    Update Cognito configuration in config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_8CsYo7mBH', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '23sn9e7fkpg5qoub8pb1v3kc0e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_HgARpdn4J', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3vu56r1tfbqpcqdgfg6lvtkmla', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: ' us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
